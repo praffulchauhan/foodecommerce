3e4b6c2fae12867ce0cb7c6dd3e3bdf15b9baeca
divyansh25-dev <divyansh25.panwar@gmail.com>
2022-04-27 13:04:44 +0530

    Nav bar added

--- react-app/src/Components/HomePage/Home.js
+++ react-app/src/Components/HomePage/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import Nav_and_logo from "./Nav_and_logo/Nav_and_logo";
 const Home = () => {
-  return <div></div>;
+  return (
+    <Nav_and_logo/>
+  );
 };
 
 export default Home;
---
+++ react-app/src/Components/HomePage/Nav_and_logo/Nav_and_logo.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Nav_and_logo.css'
+function Nav_and_logo() {
+  return (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+      <div className='margin'> <img src="https://cdn3.vectorstock.com/i/thumb-large/10/77/food-fox-logo-vector-24171077.jpg" width="70px"/></div>
+   
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="margin collapse navbar-collapse"  id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Menu</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Cart</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <div>
+        <li className=" margin2 nav-item">
+          <a className="nav-link" href="#">Login</a>
+        </li>
+ </div>
+ <div>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Sign Up</a>
+        </li>
+ </div>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Nav_and_logo

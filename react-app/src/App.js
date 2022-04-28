957e58b5a134a81be830002fdef9819e34cc6f57
antima <antimajain915@gmail.com>
2022-04-28 12:09:36 +0530

    Changed Menu Css and Resolved Console Errors

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import Admin from './Components/Admin/Admin';
 import AddProduct from './Components/Admin/AddProduct';
 import ListProduct from './Components/Admin/ListProduct';
 import Signup from './Components/Signup/Signup';
+import Menu from './Components/Menu/Menu';
 function App() {
   return (
     <div >
@@ -15,11 +16,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         
+        <Route path='/menu' element={<Menu/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/admin' element={<Admin/>}/>
         <Route path='/admin/add' element={<AddProduct/>}/>
         <Route path='/admin/list' element={<ListProduct/>}/>
         
+
       </Routes>
     </div>
 
--- react-app/src/Components/Menu/MenuC.js
+++ react-app/src/Components/Menu/MenuC.js
@@ -2,38 +2,38 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MenuC.css'
 import Quantity from './Quantity.js'
+import {Link} from "react-router-dom";;
+
 function MenuC(props) {
   return (
+    <div className='container-fluid'>
+      <div className="row">
+        {
+          props.data.map( (user,index) => (
 
-<div className="row">
-{
-props.data.map( (user,index) => (
+          <div className='col-md-4 menus'>
+            <div className="card menu-body " >
+              <img src={user.image} className="card-img-top" alt="menu"/>
+              <div className="card-body" key={index}>
+                <h5 className="card-title" >{user.name}</h5>
+                <p className="card-text">{user.description}</p>
+                <div className='dll'>
+                <p style={{fontSize:20}}><b>Rs {user.price}</b></p>
+                <Quantity/>
+                </div>
+                <Link to="" className="button cart btn btn-danger">ADD TO CART</Link>
+              </div>
+        
 
-<div className='col-md-4 pqr'>
-<div className="card lmn " >
-  <img src={user.image} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{user.name}</h5>
-    <p className="card-text">{user.description}</p>
-    <div className='dll'>
-<p style={{fontSize:20}}><b>Rs {user.price}</b></p>
 
-    <a href="#" className="flm btn btn-danger">ADD TO CART</a>
-    
+            </div>
 
-    
-</div>
-
-  </div>
-  <Quantity/>
-
-</div>
-
-</div>
+      </div>
 ))
 }
 
 
+</div>
 </div>
 
     )

30097425f3d6d4cdbb907d3d0ee8e819872af41c
himanshu2801 <spydreamer75@gmail.com>
2022-04-28 10:14:35 +0530

    introduce new changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Signup from './Components/Signup/Signup';
+import Cart from './Components/Cart/Cart';
+
 
 function App() {
-  return (
-    <div className="App">
+  
+ 
+  
+    return (
+    <div>
+      {/*<div className="App">
       <Signup/>
+  </div>*/}
+      <Cart/>
     </div>
   );
 }

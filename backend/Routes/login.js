579e3cb33b10c54200592bb77c147111beda45fb
himanshu2801 <spydreamer75@gmail.com>
2022-04-29 14:32:12 +0530

    Changes

--- backend/Routes/login.js
+++ backend/Routes/login.js
@@ -4,7 +4,9 @@ const UserService = require("../UserServices/loginService")
 const bodyParser=require("body-parser");
 var jsonParser = bodyParser.json()
 
-router.get("/",auth.required,async (req,res)=>{
+
+const auth = require("./auth")
+router.get("/",async (req,res)=>{
     const userService = new UserService();
     const result = await userService.getUser();
     res.send(result);
@@ -34,7 +36,7 @@ router.post("/login",jsonParser,async (req,res)=>{
     } 
 })
 
-router.delete(":/id",auth.required,async (req,res)=>{
+router.delete(":/id",async (req,res)=>{
     const userService = new UserService();
     const result = await userService.removeUser(req.params.id)
     res.send(result)
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require("mongoose");
 
-const port = 3000
+const port = 5000
 const cors = require("cors")
 app.use(cors())
 app.use("/user",require("./Routes/login"))
